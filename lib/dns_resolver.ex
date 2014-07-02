2dd6727e10289207717f652e25752138fbcb74f1
Frank Hunleth <fhunleth@troodon-software.com>
2014-07-02 12:19:01 -0400

    Add DNS resolv.conf manager

---
+++ lib/dns_resolver.ex
@@ -0,0 +1,87 @@
+defmodule DnsResolver do
+  use GenServer
+
+  @moduledoc """
+  This module manages the contents of "/etc/resolv.conf". This file is used
+  by the C library for resolving domain names and must be kept up-to-date
+  as links go up and down. This module assumes exclusive ownership on
+  "/etc/resolv.conf", so if any other code in the system tries to modify the
+  file, their changes will be lost on the next update.
+  """
+
+  defstruct filename: nil,
+            domains: %{},
+            nameservers: %{}
+
+  @doc """
+  """
+  def start_link(resolvconf_path \\ "/etc/resolv.conf") do
+    GenServer.start_link(__MODULE__, resolvconf_path)
+  end
+
+  def set_domain(pid, ifname, domain) do
+    GenServer.call(pid, {:set_domain, ifname, domain})
+  end
+
+  @doc """
+  Set the nameservers that were configured on this interface. These
+  will be added to "/etc/resolv.conf" and replace any entries that
+  were previously added for the specified interface.
+  """
+  def set_nameservers(pid, ifname, servers) when is_list(servers) do
+    GenServer.call(pid, {:set_nameservers, ifname, servers})
+  end
+
+  @doc """
+  Clear all entries in "/etc/resolv.conf" that are associated with
+  the specified interface.
+  """
+  def clear(pid, ifname) do
+    GenServer.call(pid, {:clear, ifname})
+  end
+
+  @doc """
+  Completely clear out "/etc/resolv.conf".
+  """
+  def clear_all(pid) do
+    GenServer.call(pid, :clear_all)
+  end
+
+  def init(filename) do
+    state = %DnsResolver{filename: filename}
+    write_resolvconf(state)
+    {:ok, state}
+  end
+
+  def handle_call({:set_domain, ifname, domain}, _from, state) do
+    newdomains = Dict.put(state.domains, ifname, domain)
+    state = %{state | domains: newdomains}
+    write_resolvconf(state)
+    {:reply, :ok, state}
+  end
+  def handle_call({:set_nameservers, ifname, nameservers}, _from, state) do
+    newnameservers = Dict.put(state.nameservers, ifname, nameservers)
+    state = %{state | nameservers: newnameservers}
+    write_resolvconf(state)
+    {:reply, :ok, state}
+  end
+  def handle_call({:clear, ifname}, _from, state) do
+    newdomains = Dict.delete(state.domains, ifname)
+    newnameservers = Dict.delete(state.nameservers, ifname)
+    state = %{state | nameservers: newnameservers, domains: newdomains}
+    write_resolvconf(state)
+    {:reply, :ok, state}
+  end
+  def handle_call(:clear_all, _from, state) do
+    state = %DnsResolver{filename: state.filename}
+    write_resolvconf(state)
+    {:reply, :ok, state}
+  end
+
+  defp write_resolvconf(state) do
+    domains = for {_ifname, domain} <- state.domains, do: "search #{domain}\n"
+    nameservers = for {_ifname, nslist} <- state.nameservers, ns <- nslist, do: "nameserver #{ns}\n"
+    File.write!(state.filename, domains ++ nameservers)
+  end
+end
+
---
+++ test/dnsresolver_test.exs
@@ -0,0 +1,69 @@
+defmodule DnsResolverTest do
+  use ExUnit.Case
+
+  @resolvconf "/tmp/test_resolv.conf"
+
+  setup do
+    {:ok, pid} = DnsResolver.start_link @resolvconf
+    on_exit(fn() -> File.rm @resolvconf end)
+    {:ok, server: pid}
+  end
+
+  test "resolvconf created" do
+    assert File.exists?(@resolvconf)
+  end
+
+  test "domain", %{server: pid} do
+    DnsResolver.set_domain(pid, "eth0", "troodon-software.com")
+    {:ok, contents} = File.read(@resolvconf)
+    assert contents == "search troodon-software.com\n"
+  end
+
+  test "domains", %{server: pid} do
+    DnsResolver.set_domain(pid, "eth0", "troodon-software.com")
+    DnsResolver.set_domain(pid, "eth1", "hunleth.com")
+    {:ok, contents} = File.read(@resolvconf)
+    assert contents == "search troodon-software.com\nsearch hunleth.com\n"
+  end
+
+  test "nameserver", %{server: pid} do
+    DnsResolver.set_nameservers(pid, "eth0", ["192.168.1.1"])
+    {:ok, contents} = File.read(@resolvconf)
+    assert contents == "nameserver 192.168.1.1\n"
+  end
+
+  test "nameservers", %{server: pid} do
+    DnsResolver.set_nameservers(pid, "eth0", ["192.168.1.1", "8.8.8.8"])
+    {:ok, contents} = File.read(@resolvconf)
+    assert contents == "nameserver 192.168.1.1\nnameserver 8.8.8.8\n"
+  end
+
+  test "nameservers2", %{server: pid} do
+    DnsResolver.set_nameservers(pid, "eth0", ["192.168.1.1", "8.8.8.8"])
+    DnsResolver.set_nameservers(pid, "wlan0", ["192.168.5.1"])
+    {:ok, contents} = File.read(@resolvconf)
+    assert contents == "nameserver 192.168.1.1\nnameserver 8.8.8.8\nnameserver 192.168.5.1\n"
+  end
+
+  test "clear one", %{server: pid} do
+    DnsResolver.set_domain(pid, "eth0", "troodon-software.com")
+    DnsResolver.set_domain(pid, "eth1", "hunleth.com")
+    DnsResolver.set_nameservers(pid, "eth0", ["192.168.1.1", "8.8.8.8"])
+    DnsResolver.set_nameservers(pid, "wlan0", ["192.168.5.1"])
+    DnsResolver.clear(pid, "eth0")
+    {:ok, contents} = File.read(@resolvconf)
+    assert contents == "search hunleth.com\nnameserver 192.168.5.1\n"
+  end
+
+  test "clear all", %{server: pid} do
+    DnsResolver.set_domain(pid, "eth0", "troodon-software.com")
+    DnsResolver.set_domain(pid, "eth1", "hunleth.com")
+    DnsResolver.set_nameservers(pid, "eth0", ["192.168.1.1", "8.8.8.8"])
+    DnsResolver.set_nameservers(pid, "wlan0", ["192.168.5.1"])
+    DnsResolver.clear_all(pid)
+    {:ok, contents} = File.read(@resolvconf)
+    assert contents == ""
+  end
+
+end
+
